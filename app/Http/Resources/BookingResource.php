7fdfba249743f9f83e8694c303d7eae280e26a3d
mais <maismokayed2@gmail.com>
2026-07-15 23:44:44 +0300

    edit bookingcontroller

--- app/Http/Resources/BookingResource.php
+++ app/Http/Resources/BookingResource.php
@@ -15,33 +15,35 @@ class BookingResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'              => $this->id,
-            'user'            => [
+            'id'   => $this->id,
+            'user' => $this->user ? [
                 'id'        => $this->user->id,
                 'full_name' => $this->user->full_name,
                 'email'     => $this->user->email,
-            ],
-            'room' => [
+            ] : null,
+
+            'room' => $this->room ? [
                 'id'              => $this->room->id,
                 'room_number'     => $this->room->room_number,
                 'type'            => $this->room->type,
                 'price_per_night' => $this->room->price_per_night,
 
-                'hotel' => [
+                'hotel' => $this->room->hotel ? [
                     'id'        => $this->room->hotel->id,
                     'name'      => $this->room->hotel->name,
                     'image_url' => $this->room->hotel->getFirstMediaUrl('images'),
-                ],
-            ],
-            'check_in_date'   => $this->check_in_date->format('Y-m-d H:i'),
-            'check_out_date'  => $this->check_out_date->format('Y-m-d H:i'),
-            'status'          => $this->status,
-            'total_price'     => $this->total_price,
-            'discount_amount' => $this->discount_amount,
-            'final_price'     => $this->final_price,
+                ] : null,
+            ] : null,
+
+            'check_in_date'    => optional($this->check_in_date)->format('Y-m-d H:i'),
+            'check_out_date'   => optional($this->check_out_date)->format('Y-m-d H:i'),
+            'status'           => $this->status,
+            'total_price'      => $this->total_price,
+            'discount_amount'  => $this->discount_amount,
+            'final_price'      => $this->final_price,
             'number_of_guests' => $this->number_of_guests,
-            'coupon_id'       => $this->coupon_id,
-            'created_at'      => $this->created_at->format('Y-m-d H:i'),
+            'coupon_id'        => $this->coupon_id,
+            'created_at'       => optional($this->created_at)->format('Y-m-d H:i'),
         ];
     }
 }
